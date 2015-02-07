a90889b773010d2fe2ed1dff133f951c0b5baea4
travis-ci <travis@travis-ci.org>
2015-02-07 04:04:01 +0000

    Deploy Travis CI build 387 to GitHub

--- demo/__init__.py
+++ demo/__init__.py
@@ -1,5 +1,7 @@
 """Package for PythonTemplateDemo."""
 
+import sys
+
 __project__ = 'PythonTemplateDemo'
 __version__ = '0.0.0'
 
@@ -7,6 +9,5 @@ VERSION = __project__ + '-' + __version__
 
 PYTHON_VERSION = 2, 7
 
-import sys
 if not sys.version_info >= PYTHON_VERSION:  # pragma: no cover (manual test)
     exit("Python {}.{}+ is required.".format(*PYTHON_VERSION))
