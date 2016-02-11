bf870372a85ad56c49578308b77997be6911630f
travis-ci <travis@travis-ci.org>
2016-02-11 01:48:45 +0000

    Deploy Travis CI build 646 to GitHub

--- demo/__init__.py
+++ demo/__init__.py
@@ -10,4 +10,4 @@ VERSION = "{0} v{1}".format(__project__, __version__)
 PYTHON_VERSION = 2, 7
 
 if sys.version_info < PYTHON_VERSION:  # pragma: no cover (manual test)
-    exit("Python {}.{}+ is required.".format(*PYTHON_VERSION))
+    sys.exit("Python {}.{}+ is required.".format(*PYTHON_VERSION))
