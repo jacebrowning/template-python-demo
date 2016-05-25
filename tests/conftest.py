ad0c6c0c5d29563389736b065cfa163d40106734
travis-ci <travis@travis-ci.org>
2016-05-25 19:23:30 +0000

    Deploy Travis CI build 713 to GitHub

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,3 @@
 """Integration tests configuration file."""
 
-from demo.test.conftest import pytest_configure  # pylint: disable=unused-import
+from demo.tests.conftest import pytest_configure  # pylint: disable=unused-import
