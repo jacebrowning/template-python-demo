5fdb08855edaf8e5e268f4217d3d2e278add5328
travis-ci <travis@travis-ci.org>
2016-08-27 16:36:42 +0000

    Deploy Travis CI build 795 to GitHub

--- demo/__init__.py
+++ demo/__init__.py
@@ -1,13 +1,6 @@
 """Package for TemplateDemo."""
 
-import sys
-
 __project__ = 'TemplateDemo'
 __version__ = '0.0.0'
 
 VERSION = "{0} v{1}".format(__project__, __version__)
-
-PYTHON_VERSION = 2, 7
-
-if sys.version_info < PYTHON_VERSION:  # pragma: no cover (manual test)
-    sys.exit("Python {}.{}+ is required.".format(*PYTHON_VERSION))
--- setup.py
+++ setup.py
@@ -2,21 +2,47 @@
 
 """Setup script for TemplateDemo."""
 
+import os
+import sys
+
 import setuptools
 
-from demo import __project__, __version__
 
-try:
-    README = open("README.rst").read()
-    CHANGELOG = open("CHANGELOG.rst").read()
-except IOError:
-    LONG_DESCRIPTION = "<placeholder>"
-else:
-    LONG_DESCRIPTION = README + '\n' + CHANGELOG
+PACKAGE_NAME = 'demo'
+MINIMUM_PYTHON_VERSION = 2, 7
 
+
+def check_python_version():
+    """Exit when the Python version is too low."""
+    if sys.version_info < MINIMUM_PYTHON_VERSION:
+        sys.exit("Python {}.{}+ is required.".format(*MINIMUM_PYTHON_VERSION))
+
+
+def read_package_variable(key):
+    """Read the value of a variable from the package without importing."""
+    module_path = os.path.join(PACKAGE_NAME, '__init__.py')
+    with open(module_path) as module:
+        for line in module:
+            parts = line.split(' ')
+            if parts and parts[0] == key:
+                return parts[-1]
+
+
+def read_descriptions():
+    """Build a description for the project from documentation files."""
+    try:
+        readme = open("README.rst").read()
+        changelog = open("CHANGELOG.rst").read()
+    except IOError:
+        return "<placeholder>"
+    else:
+        return readme + '\n' + changelog
+
+
+check_python_version()
 setuptools.setup(
-    name=__project__,
-    version=__version__,
+    name=read_package_variable('__package__'),
+    version=read_package_variable('__version__'),
 
     description="A sample project templated from jacebrowning/template-python.",
     url='https://github.com/jacebrowning/template-python-demo',
@@ -27,7 +53,7 @@ setuptools.setup(
 
     entry_points={'console_scripts': []},
 
-    long_description=LONG_DESCRIPTION,
+    long_description=read_descriptions(),
     license='MIT',
     classifiers=[
         # TODO: update this list to match your application: https://pypi.python.org/pypi?%3Aaction=list_classifiers
