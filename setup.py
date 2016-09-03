cbe587909ef94436e87018ea8d8df945bb2a9471
travis-ci <travis@travis-ci.org>
2016-09-03 00:20:43 +0000

    Deploy Travis CI build 797 to GitHub

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ def read_descriptions():
 
 check_python_version()
 setuptools.setup(
-    name=read_package_variable('__package__'),
+    name=read_package_variable('__project__'),
     version=read_package_variable('__version__'),
 
     description="A sample project templated from jacebrowning/template-python.",
