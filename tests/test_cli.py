c02215bc85e3e3f985b6290529c94543aa57a674
travis-ci <travis@travis-ci.org>
2016-09-17 22:12:47 +0000

    Deploy Travis CI build 823 to GitHub

---
+++ tests/test_cli.py
@@ -0,0 +1,19 @@
+"""Sample integration test module."""
+# pylint: disable=no-self-use,missing-docstring
+
+import unittest
+
+from click.testing import CliRunner
+
+from demo.cli import main
+
+
+class TestDemo(unittest.TestCase):
+    """Sample integration test class."""
+
+    def test_conversion(self):
+        runner = CliRunner()
+        result = runner.invoke(main, ['42'])
+
+        self.assertEqual(result.exit_code, 0)
+        self.assertEqual(result.output, "12.80165\n")
