f15ed284d1298e3a5c0b0b330fab10541f529a09
travis-ci <travis@travis-ci.org>
2016-12-08 16:43:49 +0000

    Deploy Travis CI build 859 to GitHub

--- setup.py
+++ setup.py
@@ -68,10 +68,6 @@ setuptools.setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
     ],
 
     install_requires=open("requirements.txt").readlines(),
