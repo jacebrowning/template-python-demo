553aaa12e46675e82c7a3cf93b28a667cc9d63f9
travis-ci <travis@travis-ci.org>
2022-01-30 00:02:00 +0000

    Deploy Travis CI build 1346 to GitHub

--- demo/tests/conftest.py
+++ demo/tests/conftest.py
@@ -7,5 +7,5 @@ def pytest_configure(config):
     """Disable verbose output when running tests."""
     log.init(debug=True)
 
-    terminal = config.pluginmanager.getplugin('terminal')
+    terminal = config.pluginmanager.getplugin("terminal")
     terminal.TerminalReporter.showfspath = False
